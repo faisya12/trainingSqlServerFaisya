695620e6546c84e691e30dcb1a0aa3c6173f4170
PE-D <bravianto.luhur@outlook.com>
2024-12-16 15:12:05 +0700

    no4 b

--- 4 Exercise.sql
+++ 4 Exercise.sql
@@ -24,4 +24,18 @@ GROUP BY Bulan, totalPenjualan, maxPenjualan
 	
 
 --2.Buatlah sebuah query untuk menghitung total penjualan per karyawan, termasuk hanya karyawan yang memiliki penjualan lebih besar dari rata-rata semua penjualan.
+WITH employeeSalesCTE as (
+	SELECT emp.Name, sum(a.saleAmount) as totalPenjualan
+	FROM SalesEmployees emp
+	JOIN EmployeeSales a ON emp.EmployeeID = a.EmployeeID
+	GROUP BY emp.Name
+),
 
+avgCTE as (
+	SELECT AVG(totalPenjualan) as AvgPenjualan FROM employeeSalesCTE
+)
+
+SELECT Name, totalPenjualan
+FROM employeeSalesCTE, avgCTE
+WHERE totalPenjualan > AvgPenjualan
+GROUP BY Name, totalPenjualan
